3e0c1b794d2cdb25a156eee6e1acba378f76f0ef
alit <alit@DV-IT-009>
2022-02-24 22:37:01 +0300

    Add project files.

---
+++ WebApplication3/Controllers/SehirController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class SehirController : ApiController
+    {
+        private string[] sehirler = { "Antalya", "Eskisehir", "Istanbul" };
+    //domainadresi/api/Sehir
+    public string[] Get()
+        {
+            return sehirler;
+        }
+
+        public string Get(int id)
+        {
+            return sehirler[id];
+        }
+    }
+}
